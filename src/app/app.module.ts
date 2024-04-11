5022161ae2947fceeefce1db9bf1fd7b37c99007
shreyasConnect <shreyas18.dev@gmail.com>
2024-04-11 10:26:43 +0530

    final commit

--- src/app/app.module.ts
+++ src/app/app.module.ts
@@ -1,18 +1,25 @@
-import { NgModule } from '@angular/core';
+// app.module.ts
 import { BrowserModule } from '@angular/platform-browser';
-
-import { AppRoutingModule } from './app-routing.module';
+import { NgModule } from '@angular/core';
+import { FormsModule } from '@angular/forms';
+import { AppRoutingModule } from './app-routing.module'; // Import the AppRoutingModule
 import { AppComponent } from './app.component';
+import { LoginComponent } from './login/login.component';
+import { SignupComponent } from './signup/signup.component';
 
 @NgModule({
-  declarations: [
-    AppComponent
-  ],
-  imports: [
-    BrowserModule,
-    AppRoutingModule
-  ],
-  providers: [],
-  bootstrap: [AppComponent]
+    declarations: [
+        AppComponent,
+        LoginComponent,
+        SignupComponent,
+
+    ],
+    imports: [
+        BrowserModule,
+        FormsModule,
+        AppRoutingModule // Add AppRoutingModule to the imports array
+    ],
+    providers: [],
+    bootstrap: [AppComponent]
 })
 export class AppModule { }
